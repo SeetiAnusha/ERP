a10265e172f12489b93775e6c249cde682463ebd
SeetiAnusha <seetianusha950@gmail.com>
2026-06-26 16:43:38 +0530

    added config file

---
+++ config/config.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    define: {
+      underscored: true,
+      freezeTableName: true
+    }
+  },
+  test: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    define: {
+      underscored: true,
+      freezeTableName: true
+    }
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    define: {
+      underscored: true,
+      freezeTableName: true
+    }
+  }
+};
